d57b8499e2432cdcc24fd8b9bc2a13ae80f5e3d4
Shriya <shriya.naik0309@gmail.com>
2021-03-31 23:36:19 +0530

    test

--- calculator/test_cal.c
+++ calculator/test_cal.c
@@ -1,14 +1,13 @@
 #include"unity_internals.h"
 #include"unity.h"
+#include "calculator.h"
 void setUp() {}
 void tearDown(){}
 void test_div(void)
 {
   TEST_ASSERT_EQUAL(1, div(10,10));
   TEST_ASSERT_EQUAL(0, div(10,0));
-  TEST_ASSERT_EQUAL(0, sub(10,11));
-  TEST_ASSERT_EQUAL(5, sub(10,5));
-  TEST_ASSERT_EQUAL(20, add(10,10));
+  
 }
 int test_main(void)
 {
