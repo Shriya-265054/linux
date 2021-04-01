bf3918329d3484fac7b5a2a2f2ea80ccaf30b6d1
Shriya <shriya.naik0309@gmail.com>
2021-04-01 14:12:08 +0530

    analysis of code

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -1,9 +1,15 @@
-int div (int num1, int num2)
+#include<stdio.h>
+#include"calculator.h"
+int main()
 {
+  
+}
+    int div (int num1, int num2)
+   {
     if(num2==0)
       return 0;
     return num1/num2;
-} 
+    } 
 int sub (int num1, int num2)
 {
     if(num2<=num1)
@@ -15,3 +21,5 @@ int add (int num1, int num2)
 {
     return num1+num2;
 }
+
+
